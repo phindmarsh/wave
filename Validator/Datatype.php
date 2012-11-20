aefdfb0289984b8aac5220b8c1ab41709491392c
Myq Larson <myqlarson@gmail.com>
2012-11-21 11:18:54 +1300

    Removed: unneeded check for type param

--- Validator/Datatype.php
+++ Validator/Datatype.php
@@ -3,76 +3,69 @@
 
 
 abstract class Wave_Validator_Datatype {
-	
-	
+
+
 	protected $params;
 	protected $input;
-	
+
 	const COMPARATOR_ARRAY = 1;
 	const COMPARATOR_NUMERIC = 2;
 	const COMPARATOR_STRING = 3;
-	
+
 	const LENGTH_GT = 'gt';
 	const LENGTH_LT = 'lt';
-	
+
 	const TYPE_INT = 'int';
 	const TYPE_ARRAY = 'array';
 	const TYPE_STRING = 'varchar';
-	
+
 	const MIN_LENGTH = 'min_length';
 	const MAX_LENGTH = 'max_length';
 	const MEMBER_OF  = 'member_of';
 	const UNIQUE     = 'unique';
 	const EXISTS     = 'exists';
 
-	
+
 	public function __construct($params, $input){
-		
+
 		$this->params = $params;
 		$this->input = $input;
-	
+
 	}
-	
+
 	public function validate(){
-		
+
 		if(isset($this->params[self::MIN_LENGTH]) && !$this->checkLength($this->params[self::MIN_LENGTH]))
 			return Wave_Validator::ERROR_TOO_SHORT;
-			
+
 		if(isset($this->params[self::MAX_LENGTH]) && !$this->checkLength($this->params[self::MAX_LENGTH], self::LENGTH_LT))
-			return Wave_Validator::ERROR_TOO_LONG;	
-			
+			return Wave_Validator::ERROR_TOO_LONG;
+
 		if(isset($this->params[self::MEMBER_OF]) && !$this->checkMembership($this->params[self::MEMBER_OF]))
 			return Wave_Validator::ERROR_NOT_IN_ARRAY;
-			
+
 		if(isset($this->params[self::UNIQUE]) && !$this->checkUniqueness($this->params[self::UNIQUE]))
 			return Wave_Validator::ERROR_NOT_UNIQUE;
-		
+
 		if(isset($this->params[self::EXISTS]) && !$this->checkExistance($this->params[self::EXISTS]))
 			return Wave_Validator::ERROR_NOT_EXISTS;
-	
+
 		return true;
 	}
 
 	public function sanitize(){
 		return htmlentities($this->input);
 	}
-	
-	
+
+
 	protected function checkLength($limit, $comparator = self::LENGTH_GT, $datatype = null){
 		// establish a comparator
 		if ($datatype == null) {
-			if (isset($this->params['type'])) {
-				if     ($this->params['type'] === self::TYPE_ARRAY)  { $datatype = self::COMPARATOR_ARRAY;   }
-				elseif ($this->params['type'] === self::TYPE_INT)    { $datatype = self::COMPARATOR_NUMERIC; }
-				elseif ($this->params['type'] === self::TYPE_STRING) { $datatype = self::COMPARATOR_STRING;  }
-			} else {
-				// try to guess if not specified
-				if     (is_array($this->input))   { $datatype = self::COMPARATOR_ARRAY;   }
-				elseif (is_numeric($this->input)) { $datatype = self::COMPARATOR_NUMERIC; }
-				elseif (is_string($this->input))  { $datatype = self::COMPARATOR_STRING;  }
-			}
+            if     ($this->params['type'] === self::TYPE_ARRAY)  { $datatype = self::COMPARATOR_ARRAY;   }
+            elseif ($this->params['type'] === self::TYPE_INT)    { $datatype = self::COMPARATOR_NUMERIC; }
+            elseif ($this->params['type'] === self::TYPE_STRING) { $datatype = self::COMPARATOR_STRING;  }
 		}
-		
+
 		// based on the comparator deduce the correct count to compare
 		$count = null;
 		if($datatype === self::COMPARATOR_ARRAY)
@@ -81,49 +74,49 @@ abstract class Wave_Validator_Datatype {
 			$count = $this->input;
 		else if($datatype === self::COMPARATOR_STRING)
 			$count = strlen($this->input);
-		else 
+		else
 			$count = $this->input;
-			
+
 		//if the comparator is callable (is a function), return the result of that instead, otherwise do the standard op
-		
+
 		if(is_callable($datatype))
 			return $datatype($comparator, $this->input, $limit);
-		else if($comparator == self::LENGTH_GT) 
+		else if($comparator == self::LENGTH_GT)
 			return $count >= $limit;
-		else 
+		else
 			return $count <= $limit;
-	
+
 	}
-	
+
 	protected function checkMembership($container){
 		return in_array($this->input, $container);
 	}
-	
+
 	protected function checkUniqueness($props){
-	
-		if(!isset($props['model']) || (!isset($props['property']))) 
+
+		if(!isset($props['model']) || (!isset($props['property'])))
 			throw new Wave_Exception("A class name and a column must be defined for unique field.");
 		$q = Wave_DB::get()->from($props['model'])
 				 ->where($props['property'], '=', $this->input);
-				 
+
 		$obj = $q->fetchRow();
-		
+
 		return ($obj === null);
 	}
-	
+
 	protected function checkExistance($props){
-	
-		if(!isset($props['model']) || (!isset($props['property']))) 
+
+		if(!isset($props['model']) || (!isset($props['property'])))
 			throw new Wave_Exception("A class name and a column must be defined for exists field.");
 		$q = Wave_DB::get()->from($props['model'])
 				 ->where($props['property'], '=', $this->input);
 
-		
+
 		$obj = $q->fetchRow();
-		
+
 		return ($obj !== null);
 	}
-	
+
 
 }
 
